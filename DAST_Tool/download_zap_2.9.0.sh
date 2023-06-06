09faa38a987f702a444e548231095101a567089b
Bilkouristas <baskour28@gmail.com>
2023-06-06 10:52:06 +0300

    selenium tests

--- DAST_Tool/download_zap_2.9.0.sh
+++ DAST_Tool/download_zap_2.9.0.sh
@@ -5,7 +5,7 @@ url=https://github.com/zaproxy/zaproxy/releases/download/v2.9.0/ZAP_2.9.0_Crossp
 # Linux
 wget -c $url -O temp.zip
 unzip temp.zip
-rm temp.zip
+# rm temp.zip
 
 # Windows
 # Download ZAP from `url` and extract it in the current directory.
