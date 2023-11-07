197586633d600c593a5da55e099cb375b3f22307
Bill Kour <bilkouristas@Basiless-MacBook-Pro.local>
2023-11-07 09:55:42 +0000

    test

---
+++ bash.sh
@@ -0,0 +1,7 @@
+echo "# juice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/Bilkouristas/juice.git
+git push -u origin master
